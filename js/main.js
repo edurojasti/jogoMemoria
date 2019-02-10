b4f411c56b5d97a7cfed71c09c0dc9647f0e14fa
Eduardo Rojas <edurojas.si@outlook.com>
2019-02-10 17:47:24 -0200

    commit 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//logica do game Memoria
+//@author: Eduardo Rojas
+
+const cardboard = document.getElementById("cardboard")
+const imagens = ['angular.svg', 'aurelia.svg', 'backbone.svg', 'ember.svg', 'react.svg', 'vue.js'] // <-- Array 
+
+var cardHTML = ''
+
+imagens.forEach(img => {
+    cardHTML += `
+    <div>
+        <img src="imgs/${img}"
+        <img src="imgs/js-badge.svg">
+    </div>
+   `
+})
