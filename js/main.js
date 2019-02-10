cce54078ff4f2889abb5501427a430e644d99098
Eduardo Rojas <edurojas.si@outlook.com>
2019-02-10 18:41:55 -0200

    commit 3

--- js/main.js
+++ js/main.js
@@ -9,12 +9,23 @@ let cardHTML = '';
 //bloco para criar uma div no html por cada elemento do array
 imagens.forEach(img => {
     cardHTML += `
-    <div>
-        <img src="imgs/${img}">
-        <img src="imgs/js.svg">
+    <div class="memory-card">
+        <img class="front-face" src="imgs/${img}">
+        <img class="back-face" src="imgs/js.svg">
     </div>
    `
 })
 
 //adiciona todo conteudo criado no cardHTML para a variável cardboard
-cardboard.innerHTML = cardHTML
+//(cardHTML + cardHTML = duplica o conteudo)
+cardboard.innerHTML = cardHTML + cardHTML
+
+//fim de renderização HMTL
+
+function flipcard(){
+    console.log(this)
+}
+
+
+const cards = document.querySelectorAll(".memory-card")
+cards.forEach(card => card.addEventListener('click', flipcard))
